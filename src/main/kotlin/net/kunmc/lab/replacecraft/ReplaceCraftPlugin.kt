766685ebca9fe15159710f60473c4b710ea3cd6d
stcYps <stcyps14@gmail.com>
2021-07-18 11:32:31 +0900

    [add]ふぁーすとこみっと
    とりあえずがわだけ

---
+++ src/main/kotlin/net/kunmc/lab/replacecraft/ReplaceCraftPlugin.kt
@@ -0,0 +1,9 @@
+package net.kunmc.lab.replacecraft
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ReplaceCraftPlugin: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("きどうした～")
+    }
+}
