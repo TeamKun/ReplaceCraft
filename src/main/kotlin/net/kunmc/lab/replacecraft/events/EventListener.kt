b76a8b58af884ced553fe5aa03b0be1a968baed9
stcYps <stcyps14@gmail.com>
2021-07-20 23:15:57 +0900

    [fix]バージョン変更忘れてた～

--- src/main/kotlin/net/kunmc/lab/replacecraft/events/EventListener.kt
+++ src/main/kotlin/net/kunmc/lab/replacecraft/events/EventListener.kt
@@ -17,26 +17,6 @@ class EventListener(private val plugin: ReplaceCraftPlugin): Listener {
     @EventHandler
     fun onCraft(e: CraftItemEvent) {
         if(plugin.isEnable) {
-
-//            var amount: Int = e.recipe.result.amount
-//            if(e.isShiftClick) {
-//                var max: Int = e.inventory.maxStackSize
-//                val matrix = e.inventory.matrix
-//
-//                for(i in matrix) {
-//                    if(i != null) {
-//                        if(i.type == Material.AIR) {
-//                            continue
-//                        }
-//                        val tmp = i.amount
-//                        if(tmp in 1 until max) {
-//                            max = tmp
-//                        }
-//                    }
-//                }
-//                amount *= max
-//            }
-
             Bukkit.getOnlinePlayers().forEach {
                 for(i in 1..36) {
                     if(it.inventory.getItem(i-1) != null) {
