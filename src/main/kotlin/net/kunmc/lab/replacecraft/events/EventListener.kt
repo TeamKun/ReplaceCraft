be309417572ae4f379a6afb0a52ea6139d5747bf
stcYps <stcyps14@gmail.com>
2021-07-21 19:06:36 +0900

    [update]レビュー修正
    オフハンド、カーソル、作業台のアイテム置き換えに対応

--- src/main/kotlin/net/kunmc/lab/replacecraft/events/EventListener.kt
+++ src/main/kotlin/net/kunmc/lab/replacecraft/events/EventListener.kt
@@ -2,6 +2,7 @@ package net.kunmc.lab.replacecraft.events
 
 import net.kunmc.lab.replacecraft.ReplaceCraftPlugin
 import org.bukkit.Bukkit
+import org.bukkit.GameMode
 import org.bukkit.Material
 
 import org.bukkit.event.EventHandler
@@ -17,23 +18,68 @@ class EventListener(private val plugin: ReplaceCraftPlugin): Listener {
     @EventHandler
     fun onCraft(e: CraftItemEvent) {
         if(plugin.isEnable) {
+            val resultItem = e.inventory.getItem(0)
+
+            // 作業台に配置されているアイテムの置き換え
+            for(i in 0..8) {
+                val item = e.inventory.matrix.get(i)
+
+                if(item != null && 0 < item.amount - 1) {
+                    val amount = item.amount - 1
+                    e.whoClicked.sendMessage("" + item)
+                    e.inventory.setItem(i+1, ItemStack(resultItem!!.type, checkItemAmount(resultItem, amount)))
+                }
+                else {
+                    e.inventory.setItem(i+1, ItemStack(Material.AIR))
+                }
+            }
+
             Bukkit.getOnlinePlayers().forEach {
-                for(i in 1..36) {
-                    if(it.inventory.getItem(i-1) != null) {
-                        val iItem = e.inventory.getItem(0)
-                        val item  = it.inventory.getItem(i-1)
+                if(it.gameMode != GameMode.CREATIVE && it.gameMode != GameMode.SPECTATOR) {
+                    // オフハンドのアイテム置き換え
+                    if(it.inventory.itemInOffHand.type != Material.AIR) {
+                        val iItem  = e.inventory.getItem(0)
+                        val item   = it.inventory.itemInOffHand
+                        val amount = checkItemAmount(iItem, item)
 
-                        val amount = if(iItem!!.maxStackSize < item!!.amount) {
-                            iItem.maxStackSize
-                        }
-                        else {
-                            item.amount
-                        }
+                        it.inventory.setItemInOffHand(ItemStack(iItem!!.type, amount))
+                    }
 
-                        it.inventory.setItem(i-1, ItemStack(iItem.type, amount))
+                    // インベントリ内のアイテム置き換え
+                    for(i in 1..36) {
+                        if(it.inventory.getItem(i-1) != null) {
+                            val item   = it.inventory.getItem(i-1)
+                            val amount = checkItemAmount(resultItem, item)
+
+                            it.inventory.setItem(i-1, ItemStack(resultItem!!.type, amount))
+                        }
+                    }
+
+                    // カーソルのアイテム置き換え
+                    val pInv = it.openInventory
+                    if(pInv.cursor != null) {
+                        val amount = checkItemAmount(resultItem, pInv.cursor)
+                        pInv.cursor = ItemStack(resultItem!!.type, amount)
                     }
                 }
             }
+        }
+    }
+
+    private fun checkItemAmount(iItem: ItemStack?, item: ItemStack?): Int {
+        return if(iItem!!.maxStackSize < item!!.amount) {
+            iItem.maxStackSize
+        }
+        else {
+            item.amount
+        }
+    }
+    private fun checkItemAmount(iItem: ItemStack?, amount: Int): Int {
+        return if(iItem!!.maxStackSize < amount) {
+            iItem.maxStackSize
+        }
+        else {
+            amount
         }
     }
 }
