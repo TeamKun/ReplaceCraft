0c2a1ff3c859fbdd022365d58892b956946c38ec
stcYps <stcyps14@gmail.com>
2021-07-30 17:14:07 +0900

    [fix]レビュー修正
    戦犯メッセージを翻訳するように, チェストの中身が変わらないように

--- src/main/kotlin/net/kunmc/lab/replacecraft/events/EventListener.kt
+++ src/main/kotlin/net/kunmc/lab/replacecraft/events/EventListener.kt
@@ -1,16 +1,19 @@
 package net.kunmc.lab.replacecraft.events
 
 import net.kunmc.lab.replacecraft.ReplaceCraftPlugin
-import net.kunmc.lab.replacecraft.util.sendMsg
+import net.kunmc.lab.replacecraft.util.*
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.Style
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.CraftItemEvent
+import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.ItemStack
 
 class EventListener(private val plugin: ReplaceCraftPlugin): Listener {
@@ -22,101 +25,105 @@ class EventListener(private val plugin: ReplaceCraftPlugin): Listener {
     fun onCraft(e: CraftItemEvent) {
         if(plugin.isEnable) {
             val resultItem = e.inventory.getItem(0)
-            sendMsg("" + ChatColor.GOLD + "${e.whoClicked.name}が${resultItem!!.type.name}をクラフトした！")
 
             Bukkit.getOnlinePlayers().forEach {
+                it.sendMessage(Component.text("${e.whoClicked.name}が").append(translate(it, resultItem!!.type)).append(Component.text("をクラフトした！")).style(
+                    Style.style(NamedTextColor.GOLD)))
+
                 if(it.gameMode != GameMode.CREATIVE && it.gameMode != GameMode.SPECTATOR) {
                     // クラフトインベントリ内のアイテム置き換え
                     // クラフトしたプレイヤー以外
-                    if(e.whoClicked.uniqueId != it.uniqueId) {
-                        val cInv = it.openInventory.topInventory
+                    val type = it.openInventory.topInventory.type
+                    if(type == InventoryType.WORKBENCH || type == InventoryType.CRAFTING) {
+                        if(e.whoClicked.uniqueId != it.uniqueId) {
+                            val cInv = it.openInventory.topInventory
 
-                        for(i in 1 until cInv.size) {
-                            val item = cInv.getItem(i)
-
-                            if(item != null && 0 < item.amount) {
-                                cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item)))
-                            }
-                            else {
-                                cInv.setItem(i, ItemStack(Material.AIR))
-                            }
-                        }
-                    }
-                    // クラフトをしたプレイヤー
-                    else {
-                        val cInv = it.openInventory.topInventory
-                        var amount: Int = e.recipe.result.amount
-
-                        // シフトクリックでアイテムをクラフトした場合
-                        if(e.isShiftClick) {
-                            var max: Int = e.inventory.maxStackSize
-                            val matrix = e.inventory.matrix
-
-                            // クラフトしたアイテムの個数を計算
-                            for(i in matrix) {
-                                if(i != null) {
-                                    if(i.type == Material.AIR) {
-                                        continue
-                                    }
-                                    val tmp = i.amount
-                                    if(tmp in 1 until max) {
-                                        max = tmp
-                                    }
-                                }
-                            }
-                            amount *= max
-                            // ------ここまで------
-
-                            val space: Int = getSpaceSize(resultItem, it)
-                            // 空きがない場合クラフトできないのでイベントをキャンセル
-                            if(space == 0) {
-                                e.isCancelled = true
-                                return
-                            }
-
-                            // 空きスペースよりもクラフトアイテム数が多かった場合
-                            if(space < amount) {
-                                var tmp = space % resultItem.amount
-                                it.inventory.addItem(ItemStack(resultItem.type, space - tmp))
-
-                                tmp = space / resultItem.amount
-                                for(i in 1 until cInv.size) {
-                                    val item = cInv.getItem(i)
-
-                                    if(item != null && 0 < item.amount - tmp) {
-                                        cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item.amount - tmp)))
-                                    }
-                                    else {
-                                        cInv.setItem(i, ItemStack(Material.AIR))
-                                    }
-                                }
-                            }
-                            else {
-                                it.inventory.addItem(ItemStack(resultItem.type, amount))
-                                for(i in 1 until cInv.size) {
-                                    val item = cInv.getItem(i)
-
-                                    if(item != null && 0 < item.amount - (amount / resultItem.amount)) {
-                                        cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item.amount - (amount / resultItem.amount))))
-                                    }
-                                    else {
-                                        cInv.setItem(i, ItemStack(Material.AIR))
-                                    }
-                                }
-                            }
-                        }
-                        else {
                             for(i in 1 until cInv.size) {
                                 val item = cInv.getItem(i)
 
-                                if(item != null && 0 < item.amount - 1) {
-                                    cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item.amount - 1)))
+                                if(item != null && 0 < item.amount) {
+                                    cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item)))
                                 }
                                 else {
                                     cInv.setItem(i, ItemStack(Material.AIR))
                                 }
                             }
-                            it.openInventory.cursor = resultItem
+                        }
+                        // クラフトをしたプレイヤー
+                        else {
+                            val cInv = it.openInventory.topInventory
+                            var amount: Int = e.recipe.result.amount
+
+                            // シフトクリックでアイテムをクラフトした場合
+                            if(e.isShiftClick) {
+                                var max: Int = e.inventory.maxStackSize
+                                val matrix = e.inventory.matrix
+
+                                // クラフトしたアイテムの個数を計算
+                                for(i in matrix) {
+                                    if(i != null) {
+                                        if(i.type == Material.AIR) {
+                                            continue
+                                        }
+                                        val tmp = i.amount
+                                        if(tmp in 1 until max) {
+                                            max = tmp
+                                        }
+                                    }
+                                }
+                                amount *= max
+
+                                // 空きがない場合クラフトできないのでイベントをキャンセル
+                                val space: Int = getSpaceSize(resultItem, it)
+                                if(space == 0) {
+                                    e.isCancelled = true
+                                    return
+                                }
+
+                                // 空きスペースよりもクラフトアイテム数が多かった場合
+                                if(space < amount) {
+                                    var tmp = space % resultItem.amount
+                                    it.inventory.addItem(ItemStack(resultItem.type, space - tmp))
+
+                                    tmp = space / resultItem.amount
+                                    for(i in 1 until cInv.size) {
+                                        val item = cInv.getItem(i)
+
+                                        if(item != null && 0 < item.amount - tmp) {
+                                            cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item.amount - tmp)))
+                                        }
+                                        else {
+                                            cInv.setItem(i, ItemStack(Material.AIR))
+                                        }
+                                    }
+                                }
+                                else {
+                                    it.inventory.addItem(ItemStack(resultItem.type, amount))
+                                    for(i in 1 until cInv.size) {
+                                        val item = cInv.getItem(i)
+
+                                        if(item != null && 0 < item.amount - (amount / resultItem.amount)) {
+                                            cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item.amount - (amount / resultItem.amount))))
+                                        }
+                                        else {
+                                            cInv.setItem(i, ItemStack(Material.AIR))
+                                        }
+                                    }
+                                }
+                            }
+                            else {
+                                for(i in 1 until cInv.size) {
+                                    val item = cInv.getItem(i)
+
+                                    if(item != null && 0 < item.amount - 1) {
+                                        cInv.setItem(i, ItemStack(resultItem.type, checkItemAmount(resultItem, item.amount - 1)))
+                                    }
+                                    else {
+                                        cInv.setItem(i, ItemStack(Material.AIR))
+                                    }
+                                }
+                                it.openInventory.cursor = resultItem
+                            }
                         }
                     }
 
@@ -144,8 +151,6 @@ class EventListener(private val plugin: ReplaceCraftPlugin): Listener {
                             it.inventory.setItem(i-1, ItemStack(resultItem.type, amount))
                         }
                     }
-
-
                 }
             }
         }
--- src/main/kotlin/net/kunmc/lab/replacecraft/util/Util.kt
+++ src/main/kotlin/net/kunmc/lab/replacecraft/util/Util.kt
@@ -1,8 +1,11 @@
 package net.kunmc.lab.replacecraft.util
 
-import org.bukkit.Bukkit
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.translation.GlobalTranslator
 import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 fun log(sender: CommandSender, s: String) {
     sender.sendMessage(s)
@@ -20,8 +23,11 @@ fun error(sender: CommandSender, s: String) {
     sender.sendMessage("" + ChatColor.RED + "ERROR: " + s)
 }
 
-fun sendMsg(s: String) {
-    Bukkit.getOnlinePlayers().forEach {
-        it.sendMessage("" + s)
-    }
+/**
+ * @author bun133
+ */
+fun translate(p: Player,material: Material): Component {
+    val e = Component.translatable(material.translationKey)
+    val translator = GlobalTranslator.renderer()
+    return translator.render(e, p.locale())
 }
